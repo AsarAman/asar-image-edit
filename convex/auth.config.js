c07490143aad600bf2cccd226dcf835438d593eb
AsarAman <asaraman278@gmail.com>
2025-12-25 11:06:38 +0500

    first commit

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -3,11 +3,15 @@
 export default {
   providers: [
     {
-      domain: import.meta.env.VITE_OIDC_AUTHORITY,
-      applicationID: import.meta.env.VITE_OIDC_CLIENT_ID,
+      authority: import.meta.env.VITE_OIDC_AUTHORITY,
+      client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
+      redirect_uri: `${window.location.origin}/auth/callback`,
+      response_type: "code",
+      scope: "openid profile email",
     },
   ],
 };
+
 
 
 
